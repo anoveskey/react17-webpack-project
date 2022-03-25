a1c447012757b94e8511ad1e0a6c79663b187d54
Adam Noveskey <anoveskey@users.noreply.github.com>
2022-03-25 16:51:35 -0400

    added name variable to the jsx.

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,9 @@ import ClickCounter from './ClickCounter'
 const App: React.FC = (): React.ReactElement => {
   const name = 'Adam'
 
-  console.log(name)
-
   return (
     <div>
-      <h1>Slightly less dumb React App, Day 3</h1>
+      <h1>Slightly less dumb React App, Day ... not sure? My name is {name}</h1>
       <h3>Current environment: {process.env.NODE_ENV}</h3>
       <h3>made by {process.env.name}</h3>
       <img alt="Nils and Anneli" src={belCanto} />
